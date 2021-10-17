0005c8524a1e9740c3494339b9a0ea29ed7f8301
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-17 20:26:50 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void tests() {
+        assertArrayEquals(new int[]{1, 3, 2, 5, 3}, Kata.digitize(35231));
+    }
 
+    private static class Kata {
+        public static int[] digitize(long n) {
+
+        int[] result = new int[("" + n).length()];
+        for (int i = 0; n > 0; i++) {
+            result[i] = (int)(n % 10);
+            n /= 10;
+        }
+        return result;
     }
 }
+    }
+
